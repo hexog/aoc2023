ef7a60e34be3d9fefa6effe5f975096b647434bd
Egor Nikulin <egor@egornikulin.me>
2024-03-08 17:25:18 +0400

    Day 4 Part 1

--- src/Aoc2023/Common.fs
+++ src/Aoc2023/Common.fs
@@ -1,2 +1,50 @@
 ﻿module [<AutoOpen>] Aoc2023.Common
 
+open System
+open System.Buffers
+open System.Runtime.CompilerServices
+open System.Xml
+
+[<Struct; IsByRefLike>]
+type SpanEnumerator =
+
+    val mutable input: ReadOnlySpan<char>
+    val mutable current: ReadOnlySpan<char>
+    val separators: SearchValues<char>
+
+    new(input: ReadOnlySpan<char>, separators: SearchValues<char>) =
+        { input = input
+          current = ReadOnlySpan<char>.Empty
+          separators = separators }
+
+    member this.Current = this.current
+
+    member this.MoveNext() =
+        if this.input.IsEmpty then
+            false
+        else
+
+        let nextLineIndex = this.input.IndexOfAny(this.separators)
+        if nextLineIndex < 0 then
+            this.current <- this.input
+            this.input <- ReadOnlySpan<char>.Empty
+            true
+        else
+
+        if nextLineIndex = 0 then
+            this.input <- this.input.Slice(1)
+            this.MoveNext()
+        else
+
+        this.current <- this.input.Slice(0, nextLineIndex).Trim()
+        this.input <- this.input.Slice(nextLineIndex + 1)
+        true
+
+    member this.GetEnumerator() = this
+
+
+let enumerateSpanValues (input: ReadOnlySpan<char>) (separators: SearchValues<char>) =
+    SpanEnumerator(input, separators)
+
+let private lineSearchValues = SearchValues.Create("\n")
+let enumerateSpanLines (input: ReadOnlySpan<char>) : SpanEnumerator = enumerateSpanValues input lineSearchValues
---
+++ src/Aoc2023/Day4.fs
@@ -0,0 +1,54 @@
+﻿module Aoc2023.Day4
+
+open System
+open System.Buffers
+open System.Collections.Generic
+
+let numberSeparators = SearchValues.Create(" ")
+
+let parseCardInfo (line: ReadOnlySpan<char>) (winningNumberHandler: int -> unit) (numberHandler: int -> unit) : int =
+
+    // parse header <Card X:>
+    assert line.Slice(0, 4).SequenceEqual("Card")
+    let titleSeparator = line.IndexOf(':')
+    let cardNumber = Int32.Parse(line.Slice(5, titleSeparator - 5).TrimStart())
+
+    // parse card winning numbers and numbers you have
+    let cardData = line.Slice(titleSeparator + 1)
+    let mutable readingWinningNumbers = true
+    for number in enumerateSpanValues cardData numberSeparators do
+        if number.SequenceEqual("|") then
+            readingWinningNumbers <- false
+        else
+
+        let number = Int32.Parse(number)
+        if readingWinningNumbers then
+            winningNumberHandler number
+        else
+            numberHandler number
+
+    cardNumber
+
+let calculatePoints (input: string) =
+
+    let cardWinningNumberSet = HashSet()
+    let addWinningCard (value: int) = cardWinningNumberSet.Add(value) |> ignore
+
+    let mutable pointsTotal = 0
+    let mutable cardPoints = 0
+    let safeAddWinningNumberPoints (number: int) =
+        if cardWinningNumberSet.Contains(number) then
+            cardPoints <- if cardPoints = 0 then 1 else cardPoints * 2
+        ()
+
+    let addCardPointsToTotal () =
+        if cardPoints > 0 then
+            pointsTotal <- pointsTotal + cardPoints
+            cardPoints <- 0
+        cardWinningNumberSet.Clear()
+
+    for line in enumerateSpanLines (input.AsSpan()) do
+        parseCardInfo line addWinningCard safeAddWinningNumberPoints |> ignore
+        addCardPointsToTotal ()
+
+    pointsTotal
